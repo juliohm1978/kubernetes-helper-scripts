f571dcf7c22c001a9032da026ae03ce09153de37
Julio H Morimoto <jhm@juliohm.com.br>
2018-03-04 10:58:23 -0300

    Added install.sh and uninstall.sh

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+INSTALLDIR=$1
+
+if [[ "$INSTALLDIR" == "" ]]; then
+    echo "
+Kubernetes Helper Scripts
+
+USAGE:
+
+    $(basename $0) <dir>
+
+DESCRIPTION:
+
+    Installs the current version of this project.
+    
+    $(basename $0) is meant to be executed in the source directory of its
+    current version. It copies all files from ./bin into <dir> and
+    sets permission to execute for all users.
+
+ARGUMENTS:
+
+    dir: The directory where this version of the project should be installed.
+         Ex: /usr/local/bin
+
+"
+    exit 1
+fi
+
+echo
+echo "The following files will be installed"
+for f in $(find ./bin/ -mindepth 1 ); do
+  echo "  $INSTALLDIR/$(basename $f)"
+done
+
+echo
+while true; do
+    read -p "Continue (y/n)?" yn
+    case $yn in
+        [Yy]* ) echo " "; break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+chmod +x bin/*
+sudo cp -vr bin/* /usr/local/bin/
+
+echo
+echo "Installation completed"
---
+++ uninstall.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+INSTALLDIR=$1
+
+if [[ "$INSTALLDIR" == "" ]]; then
+    echo "
+Kubernetes Helper Scripts
+
+USAGE:
+
+    $(basename $0) <dir>
+
+DESCRIPTION:
+
+    Remove an installation of the current version of this project.
+    
+    $(basename $0) is meant to be executed in the source directory of its
+    current version. It looks into ./bin, list all files that were
+    supposedly installed and removes them from <dir>. 
+
+ARGUMENTS:
+
+    dir: The directory where this version of the project was installed.
+         Ex: /usr/local/bin
+
+"
+    exit 1
+fi
+
+FILESTOREMOVE=""
+echo "The following files will be removed"
+for f in $(find ./bin/ -mindepth 1 ); do
+  echo "  $INSTALLDIR/$(basename $f)"
+  FILESTOREMOVE="$FILESTOREMOVE $INSTALLDIR/$(basename $f)"
+done
+
+echo
+while true; do
+    read -p "Continue (y/n)?" yn
+    case $yn in
+        [Yy]* ) echo " "; break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+sudo rm -vfr $FILESTOREMOVE
+
+echo "Uninstall completed"
